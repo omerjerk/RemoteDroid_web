ac9363702938f190502d6d7a63f35ea5ff5beec9
Umair Khan <omerjerk@gmail.com>
2015-04-11 21:23:45 +0530

    add primitive code
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- raw.h264.js
+++ raw.h264.js
@@ -1,5 +1,17 @@
 'use strict';
 
+var toUint8Array = function(parStr){
+  var raw = window.atob(parStr);
+  var rawLength = raw.length;
+  var array = new Uint8Array(new ArrayBuffer(rawLength));
+
+  var i;
+  for(i = 0; i < rawLength; i++) {
+    array[i] = raw.charCodeAt(i);
+  }
+  return array;    
+};
+
 var RawRender = function(url) {
 	this.url = url;
 };
