6dca5c5c2dfd7ec18eb264abfb17dcdcc7aa8ab2
Umair Khan <omerjerk@gmail.com>
2015-04-22 01:44:43 +0530

    read as array buffer instead of text
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- raw.h264.js
+++ raw.h264.js
@@ -1,23 +1,33 @@
 'use strict';
 
-var toUint8Array = function(parStr){
-  var raw = window.atob(parStr);
-  var rawLength = raw.length;
-  var array = new Uint8Array(new ArrayBuffer(rawLength));
+var toUint8Array = function(data){
+  // var raw = window.atob(parStr);
+  var rawLength = data.length;
+  // var array = new Uint8Array(new ArrayBuffer(rawLength));
+  var array = new Uint8Array(data);
 
   var i;
   for(i = 0; i < rawLength; i++) {
-    array[i] = raw.charCodeAt(i);
+    array[i] = data.charCodeAt(i);
   }
   return array;
 };
 
 var RawRenderer = function(_useWorker) {
 	// this.url = url;
+	
 	this.player = new Player({
 		useWorker : _useWorker,
 		workerFile : "Decoder.js"
 	});
+	/*
+    this.player = new Decoder({
+        "rgb" : "false"
+    });*/
+/*
+    this.onPictureDecoded = function(buffer, width, height) {
+    	console.log("on picture decoded");
+    };*/
 	this.fileReader = new FileReader();
 	var that = this;
 	this.fileReader.onload = function() {
@@ -30,10 +40,11 @@ RawRenderer.prototype.getCanvas = function() {
 };
 
 RawRenderer.prototype.render = function(data) {
-	console.log("rendering");
-	this.fileReader.readAsText(data);
+	this.fileReader.readAsArrayBuffer(data);
 };
 
 RawRenderer.prototype.onDecodeMessage = function(data) {
-	console.log(data);
+	var array = new Uint8Array(data);
+	console.log(array);
+	this.player.decode(array);
 };
